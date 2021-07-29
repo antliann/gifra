27d4bb11cf43f4aa9c7deabfde93cce90ceee00d
antliann <antonliann@gmail.com>
2021-07-29 23:55:32 +0300

    white flash bug fixed

--- App.jsx
+++ App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, TransitionPresets } from '@react-navigation/stack';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { SearchScreen, DetailsScreen } from './screens';
 
@@ -9,19 +10,21 @@ const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar barStyle="light-content" />
-      <Stack.Navigator>
-        <Stack.Screen name="SearchScreen" component={SearchScreen} options={{ headerShown: false }} />
-        <Stack.Screen
-          name="DetailsScreen"
-          component={DetailsScreen}
-          options={{
-            headerShown: false,
-            ...TransitionPresets.SlideFromRightIOS,
-          }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider style={{ backgroundColor: '#000' }}>
+      <NavigationContainer>
+        <StatusBar barStyle="light-content" />
+        <Stack.Navigator>
+          <Stack.Screen name="SearchScreen" component={SearchScreen} options={{ headerShown: false }} />
+          <Stack.Screen
+            name="DetailsScreen"
+            component={DetailsScreen}
+            options={{
+              headerShown: false,
+              ...TransitionPresets.SlideFromRightIOS,
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
