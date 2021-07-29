c0378cd5364b8a9e9ea9354adf419bbd41c1b1d6
antliann <antonliann@gmail.com>
2021-07-30 02:38:05 +0300

    search icon

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { func, string } from 'prop-types';
+import { StyleSheet, TextInput, View } from 'react-native';
 
-import { StyleSheet, TextInput } from 'react-native';
+import SearchIcon from '../assets/icons/search.svg';
 
 const Searchbar = ({ onChangeText, value }) => (
-  <TextInput
-    style={styles.input}
-    onChangeText={onChangeText}
-    value={value}
-    placeholder="Search GIPHY"
-    placeholderTextColor="rgba(255, 255, 255, 0.6)"
-  />
+  <View style={styles.inputContainer}>
+    <SearchIcon />
+    <TextInput
+      style={styles.inputText}
+      onChangeText={onChangeText}
+      value={value}
+      placeholder="Search GIPHY"
+      placeholderTextColor="rgba(255, 255, 255, 0.6)"
+    />
+  </View>
 );
 
 Searchbar.propTypes = {
@@ -19,15 +23,21 @@ Searchbar.propTypes = {
 };
 
 const styles = StyleSheet.create({
-  input: {
-    color: 'rgba(255, 255, 255, 0.87)',
-    fontSize: 17,
+  inputContainer: {
     padding: 16,
     borderWidth: 1,
     borderColor: 'rgba(255, 255, 255, 0.12)',
     borderRadius: 16,
+    margin: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  inputText: {
+    color: 'rgba(255, 255, 255, 0.87)',
+    fontSize: 17,
     marginLeft: 8,
-    marginRight: 8,
+    flex: 1,
+    fontFamily: 'SFPro',
   },
 });
 
